41dc5386ae22ba4fab421f7d2d6c713487d8bf7d
Rajeswari <raji5588@gmail.com>
2020-01-21 19:09:12 +0000

    Updates

---
+++ precache-manifest.b0871bfce108b0663a5b81eb4182f009.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2bb3076538a2c0cb78b82d8a56e358ba",
+    "url": "/QuizApp/index.html"
+  },
+  {
+    "revision": "943322ee129995293ca4",
+    "url": "/QuizApp/static/css/main.2545ea7a.chunk.css"
+  },
+  {
+    "revision": "e03f00fd77d56140d9c5",
+    "url": "/QuizApp/static/js/2.3dcf92c7.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/QuizApp/static/js/2.3dcf92c7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "943322ee129995293ca4",
+    "url": "/QuizApp/static/js/main.798e522c.chunk.js"
+  },
+  {
+    "revision": "3cd8655d228bd2b43b89",
+    "url": "/QuizApp/static/js/runtime-main.9c02988f.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/quiz-app/precache-manifest.712d24dbea844a106278fdc8b245e093.js"
+  "/QuizApp/precache-manifest.b0871bfce108b0663a5b81eb4182f009.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/quiz-app/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/QuizApp/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
