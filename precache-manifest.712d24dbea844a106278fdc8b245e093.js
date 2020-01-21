494127d834f73a37183bd2fba6bc4d0321c9e2cd
Rajeswari <raji5588@gmail.com>
2020-01-21 18:54:07 +0000

    Updates

---
+++ precache-manifest.712d24dbea844a106278fdc8b245e093.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a03389f2a56f0beaefd311c7c8b919c2",
+    "url": "/quiz-app/index.html"
+  },
+  {
+    "revision": "943322ee129995293ca4",
+    "url": "/quiz-app/static/css/main.2545ea7a.chunk.css"
+  },
+  {
+    "revision": "e03f00fd77d56140d9c5",
+    "url": "/quiz-app/static/js/2.3dcf92c7.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/quiz-app/static/js/2.3dcf92c7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "943322ee129995293ca4",
+    "url": "/quiz-app/static/js/main.798e522c.chunk.js"
+  },
+  {
+    "revision": "b34aa60059e75ccab2e9",
+    "url": "/quiz-app/static/js/runtime-main.98a27deb.js"
+  }
+]);
