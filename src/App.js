d3cf52285a9e4245d46cd8506d4a80c3418bbe4b
Rajeswari <raji5588@gmail.com>
2020-01-21 11:43:28 +0000

    1.file restructure. 2.Removed router

--- src/App.js
+++ src/App.js
@@ -1,17 +1,37 @@
-import React from 'react';
-import './App.css';
-import Home from './Home';
+import React, { useState } from 'react';
+import './css/App.css';
+import allQuestions from './allQuestions';
+import FirstFlow from './ActivityFlowTypes/FirstFlow';
+import SecondFlow from './ActivityFlowTypes/SecondFlow';
 function App() {
+  //allQuestion is the JSON data from s3
+  let allActivity = allQuestions['activities'];
+  const [activityOneClicked, setActivityOneStatus] = useState(false);
+  const [activityTwoClicked, setActivityTwoStatus] = useState(false);
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-         CAE
-        </p>
-      </header>
       <div>
-        <Home />
-
+        {
+          !activityOneClicked && !activityTwoClicked &&
+          <div>
+            <header className="App-header">
+              <p>
+               CAE
+              </p>
+            </header>
+            Error Find
+            <div><a onClick={()=>{setActivityOneStatus(true)}}>ACTIVITY ONE</a></div>
+            <div><a onClick={()=>{setActivityTwoStatus(true)}}>ACTIVITY TWO</a></div>
+          </div>
+        }
+        {
+          activityOneClicked && !activityTwoClicked &&
+          <FirstFlow questions={allActivity[0]['questions']}/>
+        }
+        {
+          !activityOneClicked && activityTwoClicked &&
+          <SecondFlow questions={allActivity[1]['questions']}/>
+        }
       </div>
     </div>
   );
